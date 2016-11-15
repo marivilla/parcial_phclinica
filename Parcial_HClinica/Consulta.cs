889136574ec56f2f1dfc440e9715935002f26802
Villa <Villa@BGH>
2016-11-14 22:47:42 -0300

    +  mi base de datos

---
+++ Parcial_HClinica/Consulta.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Parcial_HClinica
+{
+    class Consulta
+    {
+        public void RellenarDataGridConsulta()
+        {
+            SqlConnection conn;
+            SqlCommand cmd;
+            string strComandoSQL;
+            string strCadenaConexion = @"Data Source=TAMARA\SQLEXPRESS;Initial Catalog=Hospital_Ginecologia;Integrated Security=True";
+            //strComandoSQL = "Select * from Consulta;";
+            strComandoSQL = "sp_consultaRead";
+            conn = new SqlConnection(strCadenaConexion);
+
+            try
+            {
+                conn.Open();
+            }
+            catch (SqlException ex)
+            {
+                MessageBox.Show("Imposible conectar con los datos");
+                foreach (SqlError err in ex.Errors)
+                {
+                    MessageBox.Show(err.Message);
+                }
+            }
+
+            cmd = new SqlCommand(strComandoSQL, conn);
+
+            try
+            {
+                SqlDataAdapter da = new SqlDataAdapter(cmd);
+                DataTable dt = new DataTable();
+                da.Fill(dt);
+                dataGridConsulta.DataSource = dt;
+
+                idConsultaTextBox.Text = this.dataGridConsulta.CurrentRow.Cells[0].Value.ToString();
+                //idPacienteTextBoxConsulta.Text = this.dataGridConsulta.CurrentRow.Cells[1].Value.ToString();
+                //idMedicoTextBoxConsulta.Text = this.dataGridConsulta.CurrentRow.Cells[2].Value.ToString();
+                IDPacienteComboBoxConsulta.Text = this.dataGridConsulta.CurrentRow.Cells[1].Value.ToString();
+                IDMedicoComboBoxConsulta.Text = this.dataGridConsulta.CurrentRow.Cells[2].Value.ToString();
+                fechaConsultaDateTimePicker.Text = this.dataGridConsulta.CurrentRow.Cells[3].Value.ToString();
+                referenciaTextBox.Text = this.dataGridConsulta.CurrentRow.Cells[4].Value.ToString();
+                fechaUltPeriodoDateTimePicker.Text = this.dataGridConsulta.CurrentRow.Cells[5].Value.ToString();
+                motivoConsultaTextBox.Text = this.dataGridConsulta.CurrentRow.Cells[6].Value.ToString();
+                pesoTextBox.Text = this.dataGridConsulta.CurrentRow.Cells[7].Value.ToString();
+                tallaTextBox.Text = this.dataGridConsulta.CurrentRow.Cells[8].Value.ToString();
+                taTextBox.Text = this.dataGridConsulta.CurrentRow.Cells[9].Value.ToString();
+            }
+            catch (SqlException ex)
+            {
+                MessageBox.Show("Error en cosulta SQL");
+                foreach (SqlError err in ex.Errors)
+                {
+                    MessageBox.Show(err.Message);
+                }
+                conn.Close();
+            }
+        }
+    }
+}
---
+++ Parcial_HClinica/Llenado.cs
@@ -0,0 +1,144 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Parcial_HClinica
+{
+    class Llenado
+    {
+        public int RellenarLista(string strConn, ListView Lista, string strComandoSQL)
+        {
+            SqlConnection conn;
+            SqlCommand cmd;
+            SqlDataReader lector;
+            ListViewItem registro;
+            string[] strColumnas;
+            object[] objColumnas;
+            int nRegs = 0;
+            int nColumnas = 0;
+            int n = 0;
+
+            conn = new SqlConnection(strConn);
+
+            try
+            {
+                conn.Open();
+            }
+            catch (SqlException ex)
+            {
+                MessageBox.Show("Imposible conectar con los datos");
+                foreach (SqlError err in ex.Errors)
+                {
+                    MessageBox.Show(err.Message);
+                }
+                return nRegs;
+            }
+            cmd = new SqlCommand(strComandoSQL, conn);
+            cmd.CommandType = CommandType.StoredProcedure;
+
+            try
+            {
+                lector = cmd.ExecuteReader();
+            }
+            catch (SqlException ex)
+            {
+                MessageBox.Show("Error en cosulta SQL");
+                foreach (SqlError err in ex.Errors)
+                {
+                    MessageBox.Show(err.Message);
+                }
+                conn.Close();
+                return nRegs;
+            }
+
+            nColumnas = lector.FieldCount;
+            objColumnas = new object[nColumnas];
+
+            Lista.Items.Clear();
+            Lista.Columns.Clear();
+            Lista.FullRowSelect = true;
+            Lista.View = View.Details;
+
+            //Colocar nombres de columnas;
+            for (n = 0; n < nColumnas; n++)
+            {
+                Lista.Columns.Add(lector.GetName(n));
+            }
+
+            //Mostrar datos
+            if (lector.HasRows)
+            {
+                strColumnas = new string[lector.FieldCount];
+                nRegs = 0;
+
+                while (lector.Read())
+                {
+                    nRegs++;
+                    for (n = 0; n < nColumnas; n++)
+                    {
+                        if (!lector.IsDBNull(n))
+                        {
+                            strColumnas[n] = lector.GetValue(n).ToString();
+                        }
+                    }
+                    registro = new ListViewItem(strColumnas);
+                    Lista.Items.Insert(nRegs - 1, registro);
+                }
+            }
+
+            //Ajustar anchos de columna automaticamente
+            for (n = 0; n < Lista.Columns.Count; n++)
+            {
+                Lista.Columns[n].AutoResize(ColumnHeaderAutoResizeStyle.HeaderSize);
+            }
+
+            conn.Close();
+
+            //No hace falta liberar variables, el colector de basura lo hace por nosotros
+            return nRegs;
+        }
+
+
+        public void LlenarCombo(ComboBox cb, string strComandoSQL, string campo)
+        {
+
+            SqlConnection conn;
+            SqlCommand cmd;
+            SqlDataReader dr;
+
+            //conn = new SqlConnection(@"Data Source = TAMARA\SQLEXPRESS; Initial Catalog = Hospital_Ginecologia; Integrated Security = True");
+            conn = new SqlConnection(@"Data Source = BGH\SQLEXPRESS; Initial Catalog = Hospital_Ginecologia; Integrated Security = True");
+            try
+            {                
+                conn.Open();
+            }
+            catch (SqlException ex)
+            {
+                MessageBox.Show("Imposible conectar con los datos");
+                foreach (SqlError err in ex.Errors)
+                {
+                    MessageBox.Show(err.Message);
+                }
+            }
+
+            try
+            {
+                cmd = new SqlCommand(strComandoSQL, conn);
+                dr = cmd.ExecuteReader();
+                while (dr.Read())
+                {
+                    cb.Items.Add(dr[campo].ToString());
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("No se llena el ComboBox: ", ex.ToString());
+            }
+        }
+    }
+}
